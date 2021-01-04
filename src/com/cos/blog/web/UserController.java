6d5f20721537b3e54304b3693ceb3b41014c82ee
kjho1224 <kjho1224@gmail.com>
2021-01-04 11:29:31 +0900

    2. 회원가입 완료

--- src/com/cos/blog/web/UserController.java
+++ src/com/cos/blog/web/UserController.java
@@ -57,7 +57,14 @@ public class UserController extends HttpServlet {
 			dto.setPassword(password);
 			dto.setEmail(email);
 			dto.setAddress(address);
-			userService.회원가입(dto);
+			System.out.println("회원가입 : "+dto);
+			int result = userService.회원가입(dto);
+			if(result == 1) {
+				response.sendRedirect("index.jsp");
+			}else {
+				// Script.back();
+				//	window.history.back();
+			}
 		}
 	}
 
