0db203fd25151c2c62640ac4a99faa14b7a6160c
kjho1224 <kjho1224@gmail.com>
2021-01-12 10:54:05 +0900

    23. 댓글 삭제하기 및 아이디 중복확인 readonly추가

--- src/com/cos/blog/web/ReplyController.java
+++ src/com/cos/blog/web/ReplyController.java
@@ -21,47 +21,61 @@ import com.google.gson.Gson;
 public class ReplyController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public ReplyController() {
-        super();
-    }
+	public ReplyController() {
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
-	
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String cmd = request.getParameter("cmd");
 		ReplyService replyService = new ReplyService();
 		// http://localhost:8080/blog/reply?cmd=save
 		HttpSession session = request.getSession();
 
 		if (cmd.equals("save")) {
-			
+
 			BufferedReader br = request.getReader();
 			String reqData = br.readLine();
 			Gson gson = new Gson();
 			SaveReqDto dto = gson.fromJson(reqData, SaveReqDto.class);
-			System.out.println("dto : "+dto);
+			System.out.println("dto : " + dto);
 
 			CommonRespDto<Reply> commonRespDto = new CommonRespDto<>();
 			Reply reply = null;
 			int result = replyService.댓글쓰기(dto);
 
-			if(result != -1) {
+			if (result != -1) {
 				reply = replyService.댓글찾기(result);
-				commonRespDto.setStatusCode(1); //1, -1
+				commonRespDto.setStatusCode(1); // 1, -1
 				commonRespDto.setData(reply);
-			}else {
-				commonRespDto.setStatusCode(-1); //1, -1
+			} else {
+				commonRespDto.setStatusCode(-1); // 1, -1
 			}
 
-			String responseData = gson.toJson(commonRespDto); 
-			System.out.println("responseData : "+responseData);
+			String responseData = gson.toJson(commonRespDto);
+			System.out.println("responseData : " + responseData);
 			Script.responseData(response, responseData);
+		} else if (cmd.equals("delete")) {
+			int id = Integer.parseInt(request.getParameter("id"));
+			int result = replyService.댓글삭제(id);
+
+			CommonRespDto commonDto = new CommonRespDto<>();
+			commonDto.setStatusCode(result); // 1, -1
+
+			Gson gson = new Gson();
+			String jsonData = gson.toJson(commonDto);
+			// { "statusCode" : 1 }
+			Script.responseData(response, jsonData);
 		}
 	}
 
